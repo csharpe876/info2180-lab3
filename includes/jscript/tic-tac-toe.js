95e563f3d49bb6d424aa54cc2722b6f9cd3939fb
Carl Sharpe <carlsharpe87@gmail.com>
2025-09-26 13:00:55 -0700

    Check for winner

--- includes/jscript/tic-tac-toe.js
+++ includes/jscript/tic-tac-toe.js
@@ -19,5 +19,31 @@ squares.forEach(function(square, index) {
                 currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
             }
         });
+
+    square.addEventListener('mouseover', function() {
+            if (!gameState[index] && !checkWin()) {
+                square.classList.add('hover');
+            }
+        });
+
+    square.addEventListener('mouseout', function() {
+        square.classList.remove('hover');
     });
 });
+
+function checkWin() {
+    const winConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]             // Diagonals
+    ];
+    for (const condition of winConditions) {
+        const [a, b, c] = condition;
+        if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+            return true;
+        }
+    }
+    return false;
+};
+});
+
